62a5a7f4e77eb086b15b45777e99180949e61234
KJ Price <kjprice12@gmail.com>
2019-05-27 12:17:43 -0500

    Perform AHE on web classification

--- web/get_classification.py
+++ web/get_classification.py
@@ -4,6 +4,7 @@ import sys
 import time
 import base64
 import cv2
+from PIL import Image
 from keras.models import load_model
 import numpy as np
 import json
@@ -16,7 +17,14 @@ def read_json_class_names():
 CLASS_NAMES = read_json_class_names()
 print('class names: {}'.format(CLASS_NAMES))
 
-model = load_model('models/weights-improvement-2019-05-25__21:33:02.665517-10-0.97.hdf5')
+model = load_model('models/weights-improvement-2019-05-27__08:08:27.199875-03-0.85.hdf5')
+
+def create_ahe_grayscale_image(img):
+    grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4,4))
+    ahe_img = clahe.apply(grayscale_img)
+    color_ahe_img = cv2.cvtColor(img, cv2.COLOR_LAB2RGB) # really just gray but with three chanels
+    return color_ahe_img
 
 def readb64(uri):
   encoded_data = uri.split(',')[1]
@@ -31,6 +39,9 @@ while True:
 
   image = readb64(base64_image)
   image = cv2.resize(image, (150, 150))
+
+  image = create_ahe_grayscale_image(image)
+
   cv2.imwrite('screenshot.jpg', image)
   image = image / 255
   image = np.array([image])
